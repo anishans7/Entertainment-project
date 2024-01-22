8f1ca4929e2dbe6ed32c703e095c5fd3e106bec4
Ujwaldevgade <108817012+Ujwaldevgade@users.noreply.github.com>
2024-01-22 15:04:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+// client/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import MoviesPage from './components/MoviesPage';
+import TvSeriesPage from './components/TvSeriesPage';
+import TrendingPage from './components/TrendingPage';
+import BookmarkPage from './components/BookmarkPage';
+import LoginPage from './components/LoginPage';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const NavItem = ({ to, iconSrc, name }) => (
+  <li>
+    <Link to={to}>
+      <img src={iconSrc} alt={name} className="nav-icon" />
+      <span className="nav-name">{name}</span>
+    </Link>
+  </li>
+);
+
+const NavBar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <ul>
+        <NavItem to="/" iconSrc="/images/home-icon.png" name="Home" />
+        <NavItem to="/movies" iconSrc="/images/movies-icon.png" name="Movies" />
+        <NavItem to="/tv-series" iconSrc="/images/tv-series-icon.png" name="TV Series" />
+        <NavItem to="/bookmarks" iconSrc="/images/bookmark-icon.png" name="Bookmarks" />
+        <NavItem to="/login" iconSrc="/images/login-icon.png" name="Login"/>
+      </ul>
+    </nav>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<TrendingPage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/tv-series" element={<TvSeriesPage />} />
+          <Route path="/bookmarks" element={<BookmarkPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        <Footer /> {/* Include the Footer component */}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
